422890bc724db17e9d8ffee5a34741e80d5bef68
Matthew Darling <matthewjdarling@gmail.com>
2017-03-29 18:50:44 -0400

    Update to emacs-custom.el

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,5 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "apple" :family "Menlo"))))
- '(emacs-lisp-mode-default ((t (:inherit autoface-default :height 100 :family "Menlo"))) t))
+ '(eval-sexp-fu-flash ((t (:foreground "green4" :weight bold)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cider-inject-dependencies-at-jack-in nil)
+ '(cider-jack-in-auto-inject-clojure nil)
+ '(cider-lein-parameters "repl :headless")
+ '(package-selected-packages
+   (quote
+    (which-key clojure-mode wgrep-ag web-mode visual-regexp-steroids use-package undo-tree string-utils solarized-theme smex smartscan slamhound shell-pop reveal-in-osx-finder rainbow-mode rainbow-delimiters projectile powerline popwin org-present nlinum magit lorem-ipsum latest-clojure-libraries kibit-helper key-chord js2-mode ido-vertical-mode ido-ubiquitous ibuffer-vc hideshowvis ham-mode golden-ratio git-link git-gutter-fringe fold-dwim flx-ido expand-region es-mode ensime emmet-mode elisp-slime-nav datomic-snippets company-quickhelp clojure-mode-extra-font-locking cljsbuild-mode clj-refactor cider-eval-sexp-fu buffer-move browse-kill-ring beginend auto-indent-mode annoying-arrows-mode ag ace-window ace-jump-mode)))
+ '(shell-pop-shell-type
+   (quote
+    ("eshell" "*eshell*"
+     (lambda nil
+       (eshell shell-pop-term-shell)))))
+ '(shell-pop-universal-key "M-`")
+ '(shell-pop-window-position "top")
+ '(shell-pop-window-size 100))
