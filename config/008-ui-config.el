e84feadd37c21d409cfe0311963548a126febb86
Matthew Darling <matthewjdarling@gmail.com>
2018-08-29 22:13:02 -0400

    Correct popwin configuration
    When I updated to Emacs 26, a weird thing started happening: Magit
    status buffers weren't full-screen! Actually, a lot of buffers were
    appearing on the right half of the screen when they didn't do so in
    Emacs 25.
    
    At first I suspect the new Emacs 26 feature of "side windows", but I
    found that side windows had entirely different behaviour from what I
    was seeing. So that couldn't be it.
    
    I dug around the various "action" functions you can provide to
    `display-buffer`, and weirdly they all produced the same behaviour.
    
    Eventually I started putting `edebug` on the Magit buffer management
    functions, on `display-buffer` and related functions, and found a
    conditional saying something like:
    
        (if display-buffer-function
            (funcall display-buffer-function ... )
          ... )
    
    And I saw that `popwin` used as `display-buffer-function`.
    
    Except Popwin is supposed to be enabled only on things which match
    `popwin:special-display-config`, and it was activating everywhere.
    
    So I put `edebug` on a bunch of `popwin` functions, and found
    `popwin:original-display-buffer`, which is *supposed to* call the
    unmodified `display-buffer` function. Except `display-buffer` was
    checking `display-buffer-function`, sending us back to `popwin`!
    
    So basically, setting `display-buffer-function` to `popwin` is 100%
    the wrong way to do things. Now the config uses the minor mode as
    intended, and things seem to work correctly.
    
    I removed a couple seemingly useless things from the configuration, as
    well. vc-log, some shell related stuff, some stuff for Slime, etc.

--- config/008-ui-config.el
+++ config/008-ui-config.el
@@ -89,32 +89,24 @@
                                        (powerline-render rhs))))))))
 
 (use-package popwin :ensure t
-  :init (setq display-buffer-function 'popwin:display-buffer
-              popwin:special-display-config
-              '(("*Help*"  :height 30 :stick t)
-                ("*Completions*" :noselect t)
-                ("*compilation*" :noselect t)
-                ("*Messages*" :height 30)
-                ("*Occur*" :noselect t)
-                ("\\*Slime Description.*" :noselect t :regexp t :height 30)
-                ("\\*Slime Inspector.*" :regexp t :height 30)
-                ("*sldb.*":regexp t :height 30)
-                ("*magit-commit*" :noselect t :height 40 :width 80)
-                ("*magit-diff*" :noselect t :height 40 :width 80)
-                ("*magit-edit-log*" :noselect t :height 15 :width 80)
-                ("*Ido Completions*" :noselect t :height 30)
-                ("*eshell*" :height 30)
-                ("*shell*" :height 30)
-                ("\\*ansi-term\\*.*" :regexp t :height 30)
-                (".*overtone.log" :regexp t :height 30)
-                ("*gists*" :height 30)
-                ("*Kill Ring*" :height 30)
-                ("*Compile-Log*" :height 30)
-                (" *auto-async-byte-compile*" :height 14 :position bottom)
-                ("*VC-log*" :height 10 :position bottom)
-
-                ("*nREPL doc*" :height 30 :position bottom)
-                ("*nREPL error*" :height 30 :position bottom)
-                ("*nREPL inspect*" :height 20 :position bottom)
-                ("*nREPL Macroexpansion*" :height 30 :position bottom)
-                ("nREPL-tmp" :height 30 :position bottom))))
+  :init
+  (progn
+    (setq popwin:special-display-config
+          (let ((starting-config (thread-first (get 'popwin:special-display-config 'standard-value) car eval)))
+            (append (remove 'help-mode starting-config) ; Remove help-mode from the list so we can add :stick t
+                    '((help-mode :height 30 :stick t)
+                      ("*Messages*" :height 30)
+                      ("*magit-commit*" :noselect t :height 40 :width 80)
+                      ("*magit-diff*" :noselect t :height 40 :width 80)
+                      ("*magit-edit-log*" :noselect t :height 15 :width 80)
+                      ("*Completions*" :noselect t :height 30)
+                      ("*Ido Completions*" :noselect t :height 30)
+                      ("*Kill Ring*" :height 30)
+                      ("*Compile-Log*" :height 30)
+                      ("*auto-async-byte-compile*" :height 14 :position bottom)
+                      ("*nREPL doc*" :height 30 :position bottom)
+                      ("*nREPL error*" :height 30 :position bottom)
+                      ("*nREPL inspect*" :height 20 :position bottom)
+                      ("*nREPL Macroexpansion*" :height 30 :position bottom)
+                      ("nREPL-tmp" :height 30 :position bottom)))))
+    (popwin-mode 1)))
